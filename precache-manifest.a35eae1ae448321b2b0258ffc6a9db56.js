1f828471c91717ca39fb9eb649285940848307ed
Sarah Armitage <saraharmitage314@gmail.com>
2020-10-15 13:35:59 -0400

    Updates

---
+++ precache-manifest.a35eae1ae448321b2b0258ffc6a9db56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b44b1a0c1b52c1f3c98cc535a625ab",
+    "url": "/math-to-math/index.html"
+  },
+  {
+    "revision": "fac6e8d7b0ccfda00fd2",
+    "url": "/math-to-math/static/css/main.38f7cb20.chunk.css"
+  },
+  {
+    "revision": "86a58ba77b3f7cbeed93",
+    "url": "/math-to-math/static/js/2.222e8263.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/math-to-math/static/js/2.222e8263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac6e8d7b0ccfda00fd2",
+    "url": "/math-to-math/static/js/main.c80b3e8f.chunk.js"
+  },
+  {
+    "revision": "fd276389d7c5f83c7cae",
+    "url": "/math-to-math/static/js/runtime-main.11ff06dc.js"
+  }
+]);
